30518cf9d0608c9dfaead38936aa19c214541f72
Mac <Mac@Macs-MacBook-Pro.local>
2017-05-13 00:48:38 +0300

    static file loaded

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-//imports
 var express = require('express');
-var port = process.env.PORT || 3000;
+var path = require('path');
+var app = express();
 
-//create instance of express
-var app = module.exports = express();
+// Define the port to run on
+app.set('port', 3000);
 
-//Get call to return json
-app.get('/', function(req,res){
-// respond a json with ip/language/and operating system
-    res.json({ip: (req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress).split(",")[0],
-     lang: req.headers['accept-language'].split(',')[0],
-     soft: require('os').type()});
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, function(){
-   console.log("listening for port: " + port);
-})
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
