3bd49f93e94cc12790d961b8037272c8f6666f5e
Mac <Mac@Macs-MacBook-Pro.local>
2017-05-17 17:55:30 +0300

    express post and url validify added

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
+var validify = require('./validify.js');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/url')
 
 // Define the port to run on
 app.set('port', 3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/new/:OrigUrl(*)', function(req, res){
+  var longUrl = req.params.OrigUrl;
+  var shortUrl = ''; // the shortened URL we will return
+  
+  if(validify.ValidURL(longUrl)){
+    res.json({ longUrl });
+  }
+  else{
+    res.json({ error : "URL not found"});
+  }
+
+});
+
+
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
---
+++ validify.js
@@ -0,0 +1,10 @@
+function ValidURL(str) {
+  var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  if(!pattern.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+module.exports.ValidURL = ValidURL;
