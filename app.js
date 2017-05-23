f25103da6ffecdb4acd86c057c78be2783de391d
Mac <Mac@Macs-MacBook-Pro.local>
2017-05-23 17:24:47 +0300

    index added ready to deploy to heroku

--- app.js
+++ app.js
@@ -9,31 +9,27 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Urls')
 var port = process.env.PROCESS || 3000;
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/new/*', function(req, res) {
   var longUrl = req.params[0];
   // the shortened URL we will return
 
-  console.log(longUrl);
+
 
   if (validify.ValidURL(longUrl)) {
     var shortUrl = Math.floor(Math.random() * 10000000).toString();
     var data = new models({original: longUrl, shortened: shortUrl, _id: shortUrl});
     console.log("long: " + longUrl);
     console.log("short: " + shortUrl);
-    console.log("data: " + data)
+    console.log("data: " + data);
 
     data.save(function(err) {
       if (err) {
         return res.send();
       }
     });
-
     res.json(data);
-
   } else {
     res.json({error: "a complete URL format not found"});
   }
@@ -45,15 +41,14 @@ app.get('/:shortenedUrl', function(req, res) {
   models.findOne({
     "_id": shortenedUrl
   }, (err, data) => {
-    if(err){return res.send();}
+    if (err) {
+      return res.send();
+    }
     res.redirect(301, data.original)
   });
 });
-// port to be implemented
-
-
 
 // Listen for requests
-app.listen(port , function() {
+app.listen(port, function() {
   console.log('Magic happens on port ' + port);
 });
